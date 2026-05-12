f6947c41579c1a675a9108f740e00c87fca6fe6d
Tiya-0306 <tienaitsai@gmail.com>
2026-05-12 21:34:47 +0800

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '今日新聞';
-export const SITE_DESCRIPTION = '最新校園與生活資訊，每日更新。';
+export const SITE_DESCRIPTION = '使用 Astro 建立的現代化新聞網站，部署在 GitHub Pages。提供校園生活、學生成就、社會議題等多元報導，採用冰藍米白配色，打造舒適的閱讀體驗。';
