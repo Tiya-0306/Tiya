ebee10260ea379ed4a26a4e9bfd09f92cd3f4487
Tiya-0306 <tienaitsai@gmail.com>
2026-05-12 21:05:32 +0800

    Add src/consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '今日新聞';
+export const SITE_DESCRIPTION = '最新校園與生活資訊，每日更新。';
